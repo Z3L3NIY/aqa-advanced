6af8969626a1b02864a0b453cef8a090b9f3f09a
Serhii Husenko <sgusenko@gmail.com>
2024-09-11 20:40:27 +0300

    OOP is js HM

---
+++ oop_in_js/EBook.js
@@ -0,0 +1,35 @@
+import Book from './Book.js';
+
+export default class EBook extends Book {
+    constructor(title, author, publicationYear, fileExtension) {
+        super(title, author, publicationYear);
+        this._fileExtension = fileExtension;
+    }
+
+    set fileExtension(value) {
+        if (typeof value !== 'string' || value.trim() === '') {
+            throw new Error('File extension must be a valid non-empty string!');
+        }
+        this._fileExtension = value;
+    }
+
+    get fileExtension() {
+        return this._fileExtension;
+    }
+
+    printInfo() {
+        console.log(`"${this.title}", ${this.author}, ${this.publicationYear} - ${this.fileExtension}`);
+    }
+
+    static convertBookToEbook(book, fileType) {
+        if (book.constructor !== Book) {
+            throw new Error('First parameter must be of Book type!');
+        }
+
+        if (typeof fileType !== 'string' || fileType.trim() === '') {
+            throw new Error('Second parameter must be a valid non-empty string!');
+        }
+
+        return new EBook(book.title, book.author, book.publicationYear, fileType);
+    }
+}
---
+++ oop_in_js/taks1.js
@@ -0,0 +1,33 @@
+import Book from './Book.js';
+import EBook from './EBook.js';
+
+const book1 = new Book("The Lord of the Rings", "John Ronald Reuel Tolkien", 1954);
+const book2 = new Book("Contact", "Carl Sagan", 1985);
+const book3 = new Book("The Catcher in the Rye", "Jerome David Salinger", 1951);
+
+book1.printInfo();
+try {
+    book1.title = "test title";
+    book1.author = "test author";
+    book1.publicationYear = 9999;
+} catch (error) {
+    console.log(`An error occurred: ${error.message}`);
+}
+book1.printInfo();
+
+book2.printInfo();
+book3.printInfo();
+
+const ebook1 = new EBook("The Great Gatsby", "Francis Scott Key Fitzgerald", 1925, "EPUB");
+
+ebook1.printInfo();
+
+const oldestBook = Book.getOldestBook([book1, book2, book3, ebook1]);
+oldestBook.printInfo();
+
+try {
+    const newEbook = EBook.convertBookToEbook(book2, "PDF");
+    newEbook.printInfo();
+} catch (error) {
+    console.log(`An error occurred: ${error.message}`);
+}
